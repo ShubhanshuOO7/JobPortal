11f266a1d5c4c447c56f221eba57253f570f0c6c
ShubhanshuOO7 <shubhanshu5320413@gmail.com>
2026-04-29 19:36:38 +0530

    fix_remainingError

--- backend/src/routes/application.ts
+++ backend/src/routes/application.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 export const applyRouter = express.Router();
 import { userMiddleware } from "../middlewares/middleware";
-import { ApplicationStatus } from "@prisma/client";
+import * as Prisma from "@prisma/client";
 import { prisma } from "../utils/prisma";
 applyRouter.use(userMiddleware);
 interface customRequest extends Request {
@@ -145,7 +145,7 @@ applyRouter.put("/updateStatus/:id", async (req: Request, res: Response) => {
         id: Number(applicantId),
       },
       data: {
-        status: status.toLowerCase() as ApplicationStatus, // ✅ cast to enum
+        status: status.toLowerCase() as Prisma.ApplicationStatus, // ✅ cast to enum
       },
     });
 
